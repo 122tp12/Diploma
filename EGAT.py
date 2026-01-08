f4fa1acc535c5174a0688724b45d97d143f97503
122tp12 <lazokivan0@gmail.com>
2026-01-08 16:22:53 +0200

    correct epoch

--- EGAT.py
+++ EGAT.py
@@ -1,8 +1,9 @@
+from sched import scheduler
+from tabnanny import verbose
 import torch
 import torch.nn.functional as F
 from torch_geometric.nn import GATv2Conv
 import numpy as np
-
 
 class EarlyStopper:
     def __init__(self, patience=150, min_delta=0, path='tmp_best_current_model.pt'):
@@ -19,12 +20,12 @@ class EarlyStopper:
         self.early_stop = False
 
     def __call__(self, val_loss, model):
-        # Якщо loss покращився (став меншим за поточний найкращий)
-        if val_loss < (self.best_loss - self.min_delta):
-            self.best_loss = val_loss
+        current_loss = val_loss.item() if torch.is_tensor(val_loss) else val_loss
+
+        if current_loss < (self.best_loss - self.min_delta):
+            self.best_loss = current_loss
             self.counter = 0
             model.save_model(self.path)
-        # Якщо loss не покращився
         else:
             self.counter += 1
             if self.counter >= self.patience:
@@ -35,38 +36,58 @@ class EarlyStopper:
                 
 class EGAT_model(torch.nn.Module):
 
-    def __init__(self, in_channels, hidden_channels, out_channels, edge_dim, heads=4):
+    def __init__(self, in_channels, hidden_channels, out_channels, edge_dim, num_hiden_layers=1, heads=4):
         super().__init__()
         
-        self.conv1 = GATv2Conv(
-            in_channels=in_channels, 
-            out_channels=hidden_channels, 
-            heads=heads, 
+        self.convs = torch.nn.ModuleList()
+        self.bns = torch.nn.ModuleList()
+
+        # 1. Input Layer
+        # 'in_channels' -> 'hidden_channels * heads'
+        self.convs.append(GATv2Conv(
+            in_channels=in_channels,
+            out_channels=hidden_channels,
+            heads=heads,
             edge_dim=edge_dim,
             concat=True
-        )
-        self.bn1 = torch.nn.BatchNorm1d(hidden_channels * heads)
-        self.conv2 = GATv2Conv(
-            in_channels=hidden_channels * heads, 
-            out_channels=out_channels, 
-            heads=1, 
+        ))
+        self.bns.append(torch.nn.BatchNorm1d(hidden_channels * heads))
+
+        # 2. Hidden Layers
+        # hidden * heads
+        for _ in range(num_hiden_layers):
+            self.convs.append(GATv2Conv(
+                in_channels=hidden_channels * heads, # Previous layer output
+                out_channels=hidden_channels,        # Hidden out (concat=True -> *heads)
+                heads=heads,
+                edge_dim=edge_dim,
+                concat=True
+            ))
+            self.bns.append(torch.nn.BatchNorm1d(hidden_channels * heads))
+
+        # 3. Output Layer
+        # 'hidden_channels * heads' -> 'out_channels'
+        # Тут concat=False, тому heads=1 (або усереднюємо, якщо heads>1)
+        self.convs.append(GATv2Conv(
+            in_channels=hidden_channels * heads,
+            out_channels=out_channels,
+            heads=1,
             edge_dim=edge_dim,
             concat=False
-        )
-        self.optimizer = torch.optim.Adam(self.parameters(), lr=0.001, weight_decay=5e-4)
+        ))
 
     def forward(self, x, edge_index, edge_attr):
-        x = self.conv1(x, edge_index, edge_attr=edge_attr)
-        x = self.bn1(x)
-        x = F.leaky_relu(x)
-        x = F.dropout(x, p=0.6, training=self.training)
-        
-        x = self.conv2(x, edge_index, edge_attr=edge_attr)
-        
+        for i in range(len(self.convs) - 1):
+            x = self.convs[i](x, edge_index, edge_attr=edge_attr)
+            x = self.bns[i](x)      # Нормалізація
+            x = F.elu(x)
+            x = F.dropout(x, p=0.6, training=self.training)
+
+        x = self.convs[-1](x, edge_index, edge_attr=edge_attr)
         return x
+    
     def save_model(self, path):
         torch.save(self.state_dict(), path)
-
     def load_model(self, path, device):
         checkpoint = torch.load(path, map_location=device)
         if isinstance(checkpoint, dict):
@@ -82,44 +103,35 @@ def accuracy(pred_y, y):
     return ((pred_y == y).sum() / len(y)).item()
 def test(model, data):
     model.eval()
-    out = model(data.x, data.edge_index, data.edge_attr)
-    acc = accuracy(out.argmax(dim=1)[data.test_mask], data.y[data.test_mask])
+    with torch.no_grad():
+        out = model(data.x, data.edge_index, data.edge_attr)
+        acc = accuracy(out.argmax(dim=1)[data.test_mask], data.y[data.test_mask])
     return acc
 
-def train(model, data):
-    device = next(model.parameters()).device
-    weight = torch.tensor([1.0, 5.0], device=device)
-    criterion = torch.nn.CrossEntropyLoss(weight=weight)
-    optimizer = model.optimizer
-    epochs = 2000
+def train_step(model, data, criterion, optimizer):
+    model.train() # Вмикаємо режим навчання (важливо для Dropout)
     
-    early_stopper = EarlyStopper(patience=300, path='./checkpoints/tmp_best_current_model.pt')
+    # 1. Forward
+    out = model(data.x, data.edge_index, data.edge_attr)
+    
+    # 2. Loss
+    loss = criterion(out[data.train_mask], data.y[data.train_mask])
+    
+    # 3. Backward
+    optimizer.zero_grad() # Очищаємо старі градієнти
+    loss.backward()       # Рахуємо нові
+    optimizer.step()      # Оновлюємо ваги
+    
+    acc = accuracy(out[data.train_mask].argmax(dim=1), data.y[data.train_mask])
+    
+    return loss.item(), acc
 
-    model.train()
-    for epoch in range(epochs+1):
-        # Training
-        optimizer.zero_grad()
-        out = model(data.x, data.edge_index, data.edge_attr)
-        loss = criterion(out[data.train_mask], data.y[data.train_mask])
-        acc = accuracy(out[data.train_mask].argmax(dim=1), data.y[data.train_mask])
-        loss.backward()
-        optimizer.step()
-
-        # Validation
-        val_loss = criterion(out[data.val_mask], data.y[data.val_mask])
-        val_acc = accuracy(out[data.val_mask].argmax(dim=1), data.y[data.val_mask])
-
-        # Print metrics every 10 epochs
-        if(epoch % 10 == 0):
-            print(f'Epoch {epoch:>3} | Train Loss: {loss:.5f} | Train Acc: '
-                  f'{acc*100:>6.2f}% | Val Loss: {val_loss:.5f} | '
-                  f'Val Acc: {val_acc*100:.2f}%')
-            
-        early_stopper(val_loss, model)
-            
-        if early_stopper.early_stop:
-            print(f"Early stopping triggered at epoch {epoch}!")
-            model=early_stopper.load_best_model(model, device)
-            break
-          
-    return model
+@torch.no_grad()
+def validate_step(model, data, criterion):
+    model.eval()
+    
+    out = model(data.x, data.edge_index, data.edge_attr)
+    val_loss = criterion(out[data.val_mask], data.y[data.val_mask])
+    val_acc = accuracy(out[data.val_mask].argmax(dim=1), data.y[data.val_mask])
+    
+    return val_loss.item(), val_acc
--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 from genericpath import isfile
 import os
 from posixpath import join
+from turtle import st
 import matplotlib.pyplot as plt
 from typing import List, Optional
+from sympy import true
 from torch_geometric.data import Data
 from sklearn.preprocessing import StandardScaler
 import datetime
 
 import torch
 
-from EGAT import EGAT_model, train
+from EGAT import EGAT_model, EarlyStopper, train_step, validate_step
 import features
 # To save dependencies:
 # pip freeze > requirements.txt
@@ -72,20 +74,22 @@ def plot_strokes(strokes: dict, clasified):
     plt.title("Render InkML")
     plt.show()
 
-def load_batch(path, device)-> tuple[List[dict], List[dict]]:
+def _load_batch(path, device)-> tuple[List[dict], List[dict]]:
     ckpt = torch.load(path, map_location=device, weights_only=False)
     return ckpt['strokes'], ckpt['labels']
 
-def save_data_batch(data: Data, path: str):
+def _save_data_batch(data: Data, path: str):
     torch.save(data.to(torch.device('cpu')), path)
 
 def load_data_batch(path: str, device) -> Data:
     data = torch.load(path, map_location=device, weights_only=False)
     return data
 
-def process_data_batch(strokes_dict: List[dict], true_y_dict: List[dict], batch: str, device) -> Data:
+def process_data_batch(batch: str, device) -> Data:
     if os.path.exists(batch[:-3]+"_proc.pt"):
         return load_data_batch(batch[:-3]+"_proc.pt", device=device)
+
+    strokes_dict, true_y_dict=_load_batch(batch, device)
 
     x= torch.empty((0,8), dtype=torch.float)
     edge_index=torch.empty((2,0), dtype=torch.long)
@@ -100,7 +104,7 @@ def process_data_batch(strokes_dict: List[dict], true_y_dict: List[dict], batch:
         for j in list(stroke.keys()):
             stroke_list.append(stroke[j])
 
-        dict_features=features.extract_stroke_features(stroke_list, offset=offset)
+        dict_features=features.extract_stroke_features(stroke_list, offset=offset) # TODO: change from stroke_list to stroke
         x_tmp=torch.tensor(list(zip(
                 dict_features["nodes"]["length"],
                 dict_features["nodes"]["width_height_ratio"],
@@ -120,7 +124,7 @@ def process_data_batch(strokes_dict: List[dict], true_y_dict: List[dict], batch:
                 dict_features["edges_features"]["temporal_distance"],
                 dict_features["edges_features"]["centroid_dx"],
                 )), dtype=torch.float)
-        true_y_tmp=torch.tensor([true_y_part['t'+k.__str__()] for k in range(len(stroke_list))], dtype=torch.long)
+        true_y_tmp=torch.tensor([true_y_part[k] for k in stroke.keys()], dtype=torch.long)
 
 
         x=torch.cat((x, x_tmp), dim=0)
@@ -128,7 +132,10 @@ def process_data_batch(strokes_dict: List[dict], true_y_dict: List[dict], batch:
         edge_attr=torch.cat((edge_attr, edge_attt_tmp), dim=0)
         true_y=torch.cat((true_y, true_y_tmp), dim=0)
 
-        offset+=stroke_list.__len__()
+        offset+=stroke.__len__()
+
+    del strokes_dict
+    del true_y_dict
 
     data=Data(
             x=x,
@@ -137,63 +144,128 @@ def process_data_batch(strokes_dict: List[dict], true_y_dict: List[dict], batch:
             y=true_y,
         )
     
+    if data.x is None or data.edge_index is None or data.edge_attr is None:
+        raise ValueError("Data object must have 'x', 'edge_index', and 'edge_attr' attributes.")
+    
     scaler = StandardScaler()
-    data.x = torch.from_numpy(scaler.fit_transform(data.x)).float() # type: ignore
-    data.edge_attr = torch.from_numpy(scaler.fit_transform(data.edge_attr)).float()# type: ignore
+    data.x = torch.from_numpy(scaler.fit_transform(data.x)).float()
+    data.edge_attr = torch.from_numpy(scaler.fit_transform(data.edge_attr)).float()
 
     data.train_mask, data.val_mask = features.get_masks(true_y.__len__())
 
 
     data.x = data.x.to(device)
     data.edge_attr = data.edge_attr.to(device)
-    data.edge_index = data.edge_index.to(device)# type: ignore
-    data.y = data.y.to(device)# type: ignore
+    data.edge_index = data.edge_index.to(device)
+    data.y = data.y.to(device)
     data.train_mask = data.train_mask.to(device)
-    data.val_mask = data.val_mask.to(device)#TODO: cheak if works
+    data.val_mask = data.val_mask.to(device)
 
-    save_data_batch(data, batch[:-3]+"_proc.pt")
+    _save_data_batch(data, batch[:-3]+"_proc.pt")
     return data
 
-def train_model(data: Data, device, model: Optional[EGAT_model]=None)-> tuple[List[int], EGAT_model]:
+def main_train_loop(device)-> tuple[List[int], EGAT_model]:
     out_channels = 2   # Кількість класів
-    hidden_channels = 20
+    hidden_channels = 15
+    hidden_layers = 1
+    heads = 4
     
-    in_channels = data.x.size(1)# type: ignore
-    edge_dim = data.edge_attr.size(1)# type: ignore
+    path = './batches/'
+    files = [f for f in os.listdir(path) if f.endswith('_proc.pt')]
 
-    if model==None:
-        model = EGAT_model(in_channels, hidden_channels, out_channels, edge_dim)
-        model = model.to(device)
-        # recreate optimizer so optimizer state lives on same device
-        model.optimizer = torch.optim.Adam(model.parameters(), lr=0.005, weight_decay=5e-4)
+    temp_data = load_data_batch(join(path, files[0]), device)
+
+    model = EGAT_model(
+        in_channels=temp_data.x.size(1),
+        hidden_channels=hidden_channels,
+        out_channels=out_channels,
+        edge_dim=temp_data.edge_attr.size(1),
+        heads=heads,
+        num_hiden_layers=hidden_layers
+    ).to(device)
+    
+    del temp_data 
+    torch.cuda.empty_cache()
+
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.05, weight_decay=5e-4)
+    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, 
+            mode='min', 
+            factor=0.5,
+            patience=10,
+            threshold=0.0001
+            )
+    
+    criterion = torch.nn.CrossEntropyLoss(weight=torch.tensor([1.0, 5.0], device=device))
+
+    early_stopper = EarlyStopper(patience=200, path='./checkpoints/tmp_best_current_model.pt')
+    epochs = 1000
+    for epoch in range(epochs):
+        epoch_train_loss = 0
+        epoch_val_loss = 0
+        epoch_train_acc = 0
+        epoch_val_acc = 0
+        count = 0
         
-    model=train(model, data)
-    out = model(data.x, data.edge_index, data.edge_attr)
+        # --- Цикл по файлах (завантажив -> навчив -> видалив) ---
+        for file_name in files:
+            # А. Завантаження
+            full_path = join(path, file_name)
+            data = load_data_batch(full_path, device)
+            
+            # Б. Крок навчання
+            loss, acc = train_step(model, data, criterion, optimizer)
+            
+            # В. Крок валідації (одразу на цьому ж графі)
+            val_loss, val_acc = validate_step(model, data, criterion)
+            
+            # Г. Статистика
+            epoch_train_loss += loss
+            epoch_train_acc += acc
+            
+            epoch_val_loss += val_loss
+            epoch_val_acc += val_acc
+            count += 1
+            
+            del data
+        
+        avg_train_loss = epoch_train_loss / count
+        avg_train_acc = epoch_train_acc / count
+        avg_val_loss = epoch_val_loss / count
+        avg_val_acc = epoch_val_acc / count
 
-    out=out.argmax(dim=1).to(torch.device('cpu')).numpy().tolist()
+        scheduler.step(avg_val_loss)
+        early_stopper(avg_val_loss, model)
+
+        current_lr = optimizer.param_groups[0]['lr']
+        if epoch % 1 == 0:
+            print(f'Epoch {epoch:>3} | Train Loss: {avg_train_loss:.5f} | Train Acc: '
+                  f'{avg_train_acc*100:>6.2f}% | Val Loss: {avg_val_loss:.5f} | '
+                  f'Val Acc: {avg_val_acc*100:.2f}% | LR: {current_lr:.6f}')
+        
+
+        if early_stopper.early_stop:
+            print(f"Early stopping triggered at epoch {epoch}!")
+            model=early_stopper.load_best_model(model, device)
+            return model
     
+
     model.save_model(f'./checkpoints/model_chekpoint_{datetime.datetime.now().timestamp()}.pt')
 
-    return out, model
+    return model
 
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-path = './batches/'
-files = [f for f in os.listdir(path) if (isfile(join(path, f)) and f.endswith('.pt') and not f.endswith('_proc.pt'))]
-model=None
 
-for file in files:
-    strokes, true_y = load_batch(path+file, device)
-    i=0
-    for i in range(strokes.__len__()):
-        if list(set(true_y[i].values())).__len__()==3 or true_y[i].__len__()!=strokes[i].__len__():
-            plot_strokes(strokes[i], true_y[i])
+if __name__ == "__main__":
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    path = './batches/'
+    files = [f for f in os.listdir(path) if (isfile(join(path, f)) and f.endswith('.pt') and not f.endswith('_proc.pt'))]
 
-"""device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-print('Using device:', device)
-
-strokes, true_y = load_batch('batches/batch0.pt', device)
-data=process_data_batch(strokes, true_y, batch='data_batches/data_batch0.pt')
-data = data.to(device) 
-clasified, model=train_model(data, device)
-plot_strokes(strokes[0], true_y[0])
-plot_strokes(strokes[0], clasified)"""
+    for f in files:
+        strokes, true_y = _load_batch(path+f, device)
+        i=0
+        for i in range(strokes.__len__()):
+            if true_y[i].keys()!=strokes[i].keys():
+                print("Mismatch between strokes and labels keys in file:", f, "at index:", i)
+                
+        if not os.path.exists(join(path, f[:-3]+"_proc.pt")):
+            print(f"Processing {f}...")
+            process_data_batch(join(path, f), device)
